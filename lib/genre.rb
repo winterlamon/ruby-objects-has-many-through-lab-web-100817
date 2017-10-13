e1e34625426571579e385333b84367c494fbe065
github username <github email address>
2017-10-13 10:30:08 -0400

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Genre
+
+  attr_accessor :name, :songs, :artist
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    self.songs.collect {|song| song.artist}
+  end
+
+end
